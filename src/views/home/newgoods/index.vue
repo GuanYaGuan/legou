a036e8f6a835f3b9fc309c6e2e981eaacaac24b9
Guan Guan <1805923599@qq.com>
2022-09-15 11:52:32 +0800

    msg

--- src/views/home/newgoods/index.vue
+++ src/views/home/newgoods/index.vue
@@ -8,15 +8,25 @@
     />
     <div class="goods">
       <div class="goodsNav">
-        <span>综合</span>
-        <div class="price">
-          <span>价格</span>
+        <span :class="{'left':color}" @click="othera"
+          >综合</span
+        >
+        <div class="price" @click="sortPrice">
+          <span :class="`${current == 'default' ? '' : 'on'}`">价格</span>
           <span class="arror">
-            <van-icon name="arrow-up" />
-            <van-icon name="arrow-down" />
+            <van-icon
+              :color="current == 'up' ? '#b4282d' : ''"
+              name="arrow-up"
+            />
+            <van-icon
+              :color="current == 'down' ? '#b4282d' : ''"
+              name="arrow-down"
+            />
           </span>
         </div>
-        <span>分类</span>
+        <span :class="{'right':show}" @click="otherb"
+          >分类</span
+        >
       </div>
       <div>
         <div class="goodsBox" v-for="item in newgoods.data" :key="item.id">
@@ -32,27 +42,62 @@
 <script>
 import { isNewHot } from "@/api/home/goodsList";
 export default {
-
   data() {
     return {
       newgoods: [],
+      order: "",
+      current: "default", // default up down
+      color:true,
+      show:false,
     };
   },
 
   created() {
-    isNewHot({
-      isHot: ""||this.$route.params.isHot,
-      isNew: ""||this.$route.params.isNew,
-      order: "",
-    }).then((res) => {
-      // console.log(res.data);
-      this.newgoods = res.data;
-    });
+    this.init();
   },
 
   methods: {
+    init() {
+      isNewHot({
+        isHot: "" || this.$route.params.isHot,
+        isNew: "" || this.$route.params.isNew,
+        order: "" || this.order,
+      }).then((res) => {
+        // console.log(res.data);
+        this.newgoods = res.data;
+      });
+    },
+    // 点击排序价格
+    sortPrice() {
+      this.color=false
+      this.show=false
+      if (this.current == "default") {
+        this.current = "up";
+        this.order = "asc";
+      } else if (this.current == "up") {
+        this.current = "down";
+        this.order = "desc";
+      } else {
+        this.current = "up";
+        this.order = "asc";
+      }
+      this.init();
+    },
     onClickLeft() {
       this.$router.back("/home");
+    },
+    // 点击价格以外的按钮
+    othera() {
+      this.show=false
+      this.current='default'
+      this.color=true
+      this.init()
+    },
+    otherb() {
+      this.show=true
+      this.color=false
+      this.current='default'
+      this.init()
     },
   },
 };
@@ -61,7 +106,7 @@ export default {
 <style lang="scss" scoped>
 div {
   background-color: rgba(227, 224, 224, 0.185);
-  .van-nav-bar.van-hairline--bottom{
+  .van-nav-bar.van-hairline--bottom {
     background-color: #fff;
     margin: 0;
   }
@@ -73,6 +118,15 @@ div {
       align-items: flex-start;
       background-color: #fff;
       margin-bottom: 5px;
+      .on {
+        color: #b4282d;
+      }
+      .left{
+        color: #b4282d;
+      }
+      .right{
+        color: #b4282d;
+      }
       span {
         line-height: 40px;
         font-size: 14px;
@@ -93,10 +147,10 @@ div {
       }
     }
     div {
-        display: flex;
-        justify-content: space-between;
-        flex-wrap: wrap;
-        align-items: center;
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
+      align-items: center;
       .goodsBox {
         width: 185px;
         display: flex;
@@ -108,10 +162,10 @@ div {
           width: 150px;
           height: 150px;
         }
-        p{
-            width: 100%;
-            margin: 5px;
-            font-size: 12px;
+        p {
+          width: 100%;
+          margin: 5px;
+          font-size: 12px;
         }
       }
     }
